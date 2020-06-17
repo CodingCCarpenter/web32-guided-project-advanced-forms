6220d831f1ef688e83d122cdd2f5f4df1622d277
CodingCCarpenter <Christineassists@aol.com>
2020-06-17 11:55:18 -0500

    creating the schema for the form data

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,20 @@
 // Here goes the schema for the form
+import * as Yup from 'yup'
+
+const formSchema = Yup.object().shape({
+    email: Yup
+      .string()
+      .email("Must be a valid email address.")
+      .required("Must include email address."),
+    password: Yup
+      .string()
+      .min(6, "Passwords must be at least 6 characters long.")
+      .required("Password is Required"),
+    terms: Yup
+      .boolean()
+      .oneOf([true], "You must accept Terms and Conditions")
+      // required isn't required for checkboxes.
+  });
+  
+  
+export default formSchema
